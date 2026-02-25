6328c4ca60ccb1b9874ed8fa1f72419564a21a94
demo-website <shubhdeeplotra990@gmail.com>
2026-02-25 21:50:06 +0530

    dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,20 @@ import {
 
 import CivicALogin from "./Components/Pages/CivicAidLogin.jsx";
 import Admin from "./Components/Pages/Admin.jsx";
+import UserDashboard from "./Components/Pages/UserDashboard.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+<<<<<<< HEAD
       <Route path="/" element={<CivicALogin />} />
       <Route path="/admin" element={<Admin />} />
       
+=======
+      <Route path="/civiAlogin" element={<CivicALogin />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/" element={<UserDashboard />} />
+>>>>>>> 3117e45 (dashboard)
     </>
   )
 );
