6fbe972280c83796f4cd86f76440566525ef1229
demo-website <shubhdeeplotra990@gmail.com>
2026-04-03 18:18:14 +0530

    map updates

---
+++ src/data/Dustbins.js
@@ -0,0 +1,90 @@
+export const dustbinLocations = [
+{
+    id: 1,
+    name: "MBA Block (Corner)", 
+    lat: 30.85993,
+    lng: 75.86046,
+    type: "green",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 2,
+    name: "MBA Block (Corner)", 
+    lat: 30.86007,
+    lng: 75.86052,
+    type: "blue",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 3,
+    name: "Opposite MBA Park", 
+    lat: 30.860056,
+    lng: 75.86095,
+    type: "green",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 4,
+    name: "Opposite MBA Park", 
+    lat: 30.859911,
+    lng: 75.86095,
+    type: "blue",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 5,
+    name: "Near Lipton Canteen", 
+    lat: 30.859999,
+    lng: 75.86112,
+    type: "green",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 6,
+    name: "Near Lipton Canteen", 
+    lat: 30.85978,
+    lng: 75.86112,
+    type: "blue",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 7,
+    name: "Opposite Tuck Shop", 
+    lat: 30.860799,
+    lng: 75.861106,
+    type: "green",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 8,
+    name: "Opposite Tuck Shop", 
+    lat: 30.86041,
+    lng: 75.860911,
+    type: "blue",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 9,
+    name: "Football ground (Bottom Left Corner)", 
+    lat: 30.860559,
+    lng: 75.860911,
+    type: "green",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 10,
+    name: "Football ground (Bottom Left Corner)", 
+    lat: 30.86065,
+    lng: 75.861106,
+    type: "blue",
+    image: "/images/location1.jpeg"
+  },
+  {
+    id: 11,
+    name: "Happiness Canteen", 
+    lat: 32.615667,
+    lng: 74.859444,
+    type: "blue",
+    image: "/images/location1.jpeg"
+  },
+];
