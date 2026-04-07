d24bad5271ebd8ef821a6f603a61d1bf87b95383
demo-website <shubhdeeplotra990@gmail.com>
2026-04-07 19:59:16 +0530

    reset password

---
+++ src/Pages/Newpassword/Newpassword.jsx
@@ -0,0 +1,50 @@
+import { Mail, Lock, Leaf, RecycleIcon } from "lucide-react";
+import "./Newpassword.css";
+
+export default function Newpassword() {
+  return (
+    <div className="np-container">
+      <div className="np-login-card">
+        <div className="np-logo-container">
+          <div className="np-logo-circle">
+            <Lock className="np-recycle" size={45} />
+          </div>
+        </div>
+
+        <h2 className="np-title">Create New Password</h2>
+        <p className="np-subtitle">Enter a strong password for your account</p>
+
+        <div className="np-form-group1">
+          <label>New Password</label>
+          <div className="np-input-box1">
+            <Mail className="np-icon" size={25} />
+            <input type="password" placeholder="Enter your password" />
+          </div>
+        </div>
+        <div className="np-form-group1">
+          <label>Confirm New Password</label>
+          <div className="np-input-box1">
+            <Lock className="np-icon" size={25} />
+            <input type="password" placeholder="Enter your password again" />
+          </div>
+        </div>
+        <button className="np-login-btn"><Lock className="np-btn-icon" size={15} />Reset Password</button>
+        <hr class="np-divider" />
+        <div className="np-footer-text">
+          <div className="np-footer-leaf1">
+            <Leaf className="np-footer-leaf" size={18} />
+          </div>
+          <div className="np-requirements">
+            <h2>Password Requirements</h2>
+
+            <p>• At least 8 characters</p>
+            <p>• One uppercase letter (recommended)</p>
+            <p>• One number (recommended)</p>
+            <p>• One special character (recommended)</p>
+          </div>
+        </div>
+        <div className="last-footer-text"> <RecycleIcon className="last-footer-leaf" size={13}/>EcoWaste • Secure Password Reset</div>
+      </div>
+    </div>
+  );
+}
