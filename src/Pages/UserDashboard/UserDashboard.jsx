19031e716b1e22a20c2f054a64e9e20c12ae19e1
Opinder-singh19 <princesingh7589884958@gmail.com>
2026-04-09 17:42:09 +0530

    testing

--- src/Pages/UserDashboard/UserDashboard.jsx
+++ src/Pages/UserDashboard/UserDashboard.jsx
@@ -9,6 +9,7 @@ import {
   BookOpen,
   MapPin,
 } from "lucide-react";
+
 export default function Userdashboard() {
   return (
     <div className="user-container">
@@ -32,7 +33,9 @@ export default function Userdashboard() {
           </button>
         </div>
       </div>
+
       <div className="user-main">
+       
         <div className="user-main1">
           <div className="user-main1-whole">
             <div className="user-main1-inner">
@@ -111,6 +114,7 @@ export default function Userdashboard() {
             </div>
           </div>
         </div>
+      
         <div className="user-map-section">
           <DustbinMap className="dustbin-main" />
         </div>
