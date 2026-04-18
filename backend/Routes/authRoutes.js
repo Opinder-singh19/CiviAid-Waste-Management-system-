7c5923a70b4822ea1d341f49c4d31bbbcb75a634
Opinder-singh19 <princesingh7589884958@gmail.com>
2026-04-18 11:11:36 +0530

    complaint logic

--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 
-const { loginUser, checkAuth, logoutUser ,signupUser,forgotPassword, verifyOtp,resetPassword } = require("../Controllers/authController");
+const { loginUser, checkAuth, logoutUser ,signupUser,forgotPassword, verifyOtp,resetPassword ,UserComplaints,mycomplaints} = require("../Controllers/authController");
 
 router.post("/login", loginUser);
 
@@ -15,5 +15,7 @@ router.post("/signup", signupUser);
 router.post("/forgot-password", forgotPassword);
 router.post("/verify-otp", verifyOtp);
 router.post("/reset-password", resetPassword);
+router.post("/User-Complaint",UserComplaints)
+router.get("/my-complaints",mycomplaints)
 
 module.exports = router;
