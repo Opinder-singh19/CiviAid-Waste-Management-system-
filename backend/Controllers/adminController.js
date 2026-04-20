f2ddc023249f5929ed2bdf005c571b391b6cc506
Opinder-singh19 <princesingh7589884958@gmail.com>
2026-04-20 17:02:08 +0530

    admin side

--- backend/Controllers/adminController.js
+++ backend/Controllers/adminController.js
@@ -10,8 +10,39 @@ const checkAdmin = (req, res, next) => {
 
 //Admin login logic here 
 
-exports.loginAdmin= async (req,res)=>{
-  const[email,password]=req.body
-  const db=getDB();
+exports.loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+     console.log("Body:", req.body);
+
+    const db = getDB();
+
+  const admin = await db.collection("admin").findOne({ email });
+    console.log("Admin from DB:", admin);
+
+    if (!admin) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    if (admin.password !== password) {
+      return res.status(401).json({ message: "Wrong password" });
+    }
+
+    req.session.admin = {
+      id: admin._id,
+      role: admin.role
+    };
+
+    res.json({
+      message: "Login success",
+      role: admin.role
+    });
+
+  } catch (err) {
+    console.log("Admin login error:", err);
+    res.status(500).json({ message: "Server error" });
+
+  }
   
-}
+
+};
--- backend/Routes/adminRoutes.js
+++ backend/Routes/adminRoutes.js
@@ -5,3 +5,4 @@ const router = express.Router();
 const{loginAdmin}= require("../Controllers/adminController");
 router.post("/login", loginAdmin);
 
+module.exports = router;
