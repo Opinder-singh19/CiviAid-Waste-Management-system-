d7abde6c6f2279d4fde68f128364bac47ef30017
demo-website <shubhdeeplotra990@gmail.com>
2026-05-10 23:07:52 +0530

    add

--- src/Components/Rewards/rewardUtils.js
+++ src/Components/Rewards/rewardUtils.js
@@ -1,10 +1,10 @@
 export const showReward = (
-  setRewardType,
   type
 ) => {
-  setRewardType(type);
 
-  setTimeout(() => {
-    setRewardType(null);
-  }, 5000);
+  localStorage.setItem(
+    "rewardPopup",
+    type
+  );
+
 };
